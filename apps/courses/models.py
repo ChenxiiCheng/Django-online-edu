5c0d29d809805de93e093124739d2af5c0c110ba
ChenxiiCheng <chenxic1011@gmail.com>
2019-03-19 18:05:08 -0400

    modified apps/courses/models.py->category

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -21,7 +21,7 @@ class Course(models.Model):
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏人数")
     image = models.ImageField(upload_to="courses/%Y/%m", verbose_name=u"封面图", max_length=100)
     click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
-    category = models.CharField(default=u"后端开发", max_length=20, verbose_name=u"课程类别")
+    category = models.CharField(default="", max_length=50, blank=True, null=True, verbose_name=u"课程类别")
     tag = models.CharField(default="", verbose_name=u"课程标签", max_length=10)
     youneed_know = models.CharField(default="", max_length=300, verbose_name=u"课程须知")
     teacher_tell = models.CharField(default="", max_length=300, verbose_name=u"老师告诉你")
